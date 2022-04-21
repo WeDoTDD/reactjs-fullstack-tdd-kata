d3d8e22bcb58cf4a6ee8516d2ab6e6bf1459fdd8
Venkatesh <vyakkant@ford.com>
2022-04-21 16:05:25 -0400

    wip

--- src/test/client/react-testing-lib-jest/roasters.helloworld.spec.js
+++ src/test/client/react-testing-lib-jest/roasters.helloworld.spec.js
@@ -5,32 +5,6 @@ import { render, waitFor } from '@testing-library/react';
 import React from 'react';
 import Roasters, { RoasterList } from '../../../client/react-testing-lib/views/Roasters';
 
-describe('Roasters - Hello World', () => {
-	it('shows the text "Hello World"', async () => {
-		const roastersData = { roasters: 'Hello World' };
-		const fetchRoasters = async () => roastersData;
-
-		let roasterList;
-		const roasters = render(<Roasters fetchRoasters={fetchRoasters} />);
-		await waitFor(() => {
-			roasterList = roasters.queryByTestId('roasters');
-			expect(roasterList.innerHTML).toEqual('Hello World');
-		});
-	});
-
-	it('shows no roasters when none exist', async () => {
-		const fetchRoasters = async () => null;
-		const roasters = render(<RoasterList getRoasters={fetchRoasters} />);
-
-		let roasterList;
-		await waitFor(() => {
-			roasterList = roasters.queryByTestId('roasters');
-		});
-
-		expect(roasterList).toBeNull();
-	});
-});
-
 describe('Show a list of Coffee Roasters', () => {
 	it('shows no roasters when none exist', async () => {
 		const fetchRoasters = async () => null;
@@ -45,14 +19,14 @@ describe('Show a list of Coffee Roasters', () => {
 	});
 			
 	it('shows one roaster ', async () => {
-		// arrange
+
+		const firstRoaster = 'First Roaster';
 		const roasterData = { roasters: 'First Roaster' };
 		const fetchRoasters = async () => roasterData;
 
-		// act
 		let roasterList;
 		const roaster = render(<Roasters fetchRoasters={fetchRoasters} />);
-		// assert
+
 		await waitFor(() => {
 			roasterList = roaster.queryByTestId('roasters');
 			expect(roasterList.innerHTML).toEqual('First Roaster');
