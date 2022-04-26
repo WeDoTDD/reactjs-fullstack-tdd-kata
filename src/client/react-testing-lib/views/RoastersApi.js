5ff97af249a93cc4d42630b754136a624ee2c9bf
Venkatesh <vyakkant@ford.com>
2022-04-26 15:36:12 -0400

    refactor the code1

--- src/client/react-testing-lib/views/RoastersApi.js
+++ src/client/react-testing-lib/views/RoastersApi.js
@@ -1,13 +1,7 @@
-import request from 'superagent';
 
-const url = 'http://localhost:4000/graphql';
+
 
 export async function fetchRoasters() {
 	return { roasters: 'First Roaster' };
 }
 
-
-async function sendRequest(query) {
-	const response = await request.post(url).send(query);
-	return response;
-}
